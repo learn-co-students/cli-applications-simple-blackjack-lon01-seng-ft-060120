9265788c397bb399303538859218ede4e951d725
Daniel Andrew <dwandrew@hotmail.com>
2020-04-08 09:58:10 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,80 +1,70 @@
 def welcome
   puts "Welcome to the Blackjack Table"
-  # code #welcome here
 end
 
 def deal_card
   rand(1..11)
-  # code #deal_card here
 end
 
 def display_card_total(num)
   puts "Your cards add up to #{num}"
-  # code #display_card_total here
 end
 
 def prompt_user
   puts "Type 'h' to hit or 's' to stay"
-  # code #prompt_user here
 end
 
 def get_user_input
   gets.chomp
-  # code #get_user_input here
 end
 
-def end_game(final)
-  puts "Sorry, you hit #{final}. Thanks for playing!"
-  # code #end_game here
+def end_game(num)
+if num == 21
+  puts "BLACKJACK!"
+else
+  puts "Sorry, you hit #{num}. Thanks for playing!"
+end
 end
 
 def initial_round
-  total=deal_card
-  total +=deal_card
-  display_card_total(total)
-  total
- 
-  # code #initial_round here
+total =0
+total +=deal_card
+total += deal_card
+
+display_card_total(total)
+total
+
 end
-
-
-
-def hit?(num)
-  total= num
-  prompt_user
-  input = get_user_input
-    if input== 's'
-      total += 0 
-    elsif input == 'h'
-      total+= deal_card
-    elsif input != 'h' || 's' 
-      invalid_command
-      prompt_user
-      get_user_input
-      end
-  total
-  # code hit? here
-end
-#end
 
 def invalid_command
-  puts "Please enter a valid command"
-  
-  # code invalid_command here
+  puts 'Please enter a valid command'
 end
 
-#####################################################
-# get every test to pass before coding runner below #
-#####################################################
+def hit?(num)
+  prompt_user
+  input =get_user_input
+  if input == 'h'
+    num += deal_card;
+    return num
+  else if input =='s'
+    return num;
+  else
+    invalid_command;
+   prompt_user;
+   get_user_input;
+  end
+end
+end
 
 def runner
   welcome
   total = initial_round
-  if total <= 21
+  until total >= 21
   total= hit?(total)
   display_card_total(total)
   end
+  if total == 21
   end_game(total)
-  # code runner here
+  end
+  end_game(total)
 end
-    
